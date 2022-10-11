e5b64c9e53046c2aae1267af3a34610446a600d0
Andrew Buvria <buvrya228@gmail.com>
2022-10-11 22:35:06 +0300

    feat: lazy loading routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'student',
+    loadChildren: () =>
+      import('./components/student/student.module').then((mod) => mod.StudentModule),
+  },
+  {
+    path: 'teacher',
+    loadChildren: () =>
+      import('./components/teacher/teacher.module').then((mod) => mod.TeacherModule),
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
