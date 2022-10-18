d127e2fbda49ffccb95495a6beb8b423289c7777
Andrew <buvrya228@gmail.com>
2022-10-18 18:43:56 +0300

    feat: provide validation for controls

--- src/app/components/base/auth/login/login.component.ts
+++ src/app/components/base/auth/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -9,18 +10,28 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 export class LoginComponent implements OnInit {
 
   public hide: boolean = true;
-  public form!: FormGroup
+  public form!: FormGroup;
 
-  constructor() { }
+  public validatorsLength: { firstname: number } = {
+    firstname: 20,
+  };
+
+  constructor(private router: Router, private route: ActivatedRoute) { }
 
   ngOnInit(): void {
     this.form = new FormGroup({
-      username: new FormControl('', [Validators.required, Validators.maxLength(20)]),
+      username: new FormControl('', [Validators.required, Validators.maxLength(this.validatorsLength.firstname)]),
       password: new FormControl('', [Validators.required]),
     });
   }
 
+  public goSignup() {
+    this.router.navigate(['../signup'], { relativeTo: this.route });
+  }
+
   public onSubmit() {
+    if (this.form.invalid) return;
+
     const data = {
       username: this.form.get('username')!.value,
       password: this.form.get('password')!.value
--- src/app/components/base/auth/signup/signup.component.ts
+++ src/app/components/base/auth/signup/signup.component.ts
@@ -1,4 +1,7 @@
+import { ActivatedRoute, Router } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+
 
 @Component({
   selector: 'app-signup',
@@ -7,9 +10,58 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignupComponent implements OnInit {
 
-  constructor() { }
+  public hide: boolean = true;
+  public form!: FormGroup;
+
+  public validatorsLength: { firstname: number, username: number,} = {
+    firstname: 36,
+    username: 20
+  };
+
+  constructor(private router: Router, private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.form = new FormGroup({
+      firstname: new FormControl('', [Validators.required, Validators.maxLength(this.validatorsLength.firstname)]),
+      username: new FormControl('', [Validators.required, Validators.maxLength(this.validatorsLength.username)]),
+      email: new FormControl('', [Validators.required, Validators.email]),
+      about: new FormControl('', [Validators.required]),
+      password: new FormControl('', [Validators.required]),
+      re_password: new FormControl('', [Validators.required]),
+    });
+  }
+
+  public goLogin() {
+    this.router.navigate(['../login'], { relativeTo: this.route });
+  }
+
+  public onSubmit() {
+    if (this.form.invalid) return;
+
+    const data = {
+      firstname: this.form.get('firstname')!.value,
+      username: this.form.get('username')!.value,
+      email: this.form.get('email')!.value,
+      about: this.form.get('about')!.value,
+      password: this.form.get('password')!.value,
+    };
+
+    console.log(data);
+
+    this.form.reset();
+  }
+
+  /* Errors */
+  public getEmailError() {
+    if (this.form.get('email')?.hasError('required')) return 'Field must be filled';
+
+    return this.form.get('email')?.hasError('email') ? 'Not a valid email' : '';
+  }
+
+  public getPasswordError() {
+    if (this.form.get('re_password')?.hasError('required')) return 'Field must be filled';
+
+    return this.form.get('password')?.value != this.form.get('re_password')?.value ? 'Password does not match' : '';
   }
 
 }
